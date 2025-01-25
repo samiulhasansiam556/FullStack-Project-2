d39c0f517f5a5054ba704285e1f5fb05fd5c121c
Samiul Hasan Siam <156831178+samiulhasansiam556@users.noreply.github.com>
2025-01-25 23:20:19 +0600

    Specify Node.js version in engines

--- Server/controllers/webhookHandler.js
+++ Server/controllers/webhookHandler.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 export const stripeWebhookHandler = async (req, res) => {
   // Log the raw body for debugging
 //   console.log("Raw Body:", req.body.toString());
-  // console.log("Webhook Handler Invoked");
+   console.log("Webhook Handler Invoked");
 
   const sig = req.headers["stripe-signature"]; // Retrieve Stripe signature from headers
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET; // Your webhook secret
